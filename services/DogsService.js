62889b749ad817f75ab7911fd5ed70f137d88290
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2019-05-14 17:01:02 -0600

    privatize schema

--- services/DogsService.js
+++ services/DogsService.js
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose'
+
+let _schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  sound: { type: String, required: true }
+ })
+
 export default class DogsService {
-  get schema() {
-    return new mongoose.Schema({
-      name: { type: String, required: true },
-      sound: { type: String, required: true }
-    })
-  }
-
-  //don't fully grasp what's happening here.
-  //looks like a getter that's instantiating a model, we're passing in a name for the model and the schema for that model. i'm a little lost though.
-
   get repository() {
-    return mongoose.model("dog", this.schema)
+    return mongoose.model("dog", _schema)
   }
 }
